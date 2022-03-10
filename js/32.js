51fd9cfa73897a9bed347efab730f03982ac86f9
emi2102 <programadoraemil@gmail.com>
2022-03-09 21:26:07 -0400

    actualizacion de las clases hasta 32.js y readme

--- js/32.js
+++ js/32.js
@@ -6,18 +6,38 @@ function descargarNuevosClientes(){
     console.log("Descargando clientes... espere...");
     setTimeout( ()=>{// esperamos 5 segundos
        resolve('Los clientes fueron descargados');
-    },5000);
-    
+    },5000); 
 });
+}
 
+function descargarUltimosPedidos(){
+   return new Promise (resolve =>{
+    console.log("Descargando pedidos... espere...");
+    setTimeout( ()=>{// esperamos 5 segundos
+       resolve('Los pedidos fueron descargados');
+    },3000); 
+});
 }
 
 
 async function app(){
-    console.log('app lista');
+    try{
+        // const clientes=await descargarNuevosClientes();
+        // const pedidos= await descargarUltimosPedidos();
+        // console.log(clientes);
+        // console.log(pedidos);
+
+        const resultado=await Promise.all([descargarNuevosClientes(), descargarUltimosPedidos()]);
+        console.log(resultado[0]);
+        console.log(resultado[1]);
+    }catch(error){
+        console.log('error');
+    }
 }
 
 app();
+
+// console.log("este codigo no se bloquea");
 // setTimeout( function(){// esperamos 5 segundos
 //     console.log("set Timeout");
 // },5000);
